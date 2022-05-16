8420f27ccb8e2cc684f35d2ef9669f083f748acf
Pat Barfield <patrickhbarfield@gmail.com>
2022-05-16 11:32:48 -0500

    rest-blog 3-controllers complete

---
+++ src/main/java/HelloController/HelloController.java
@@ -0,0 +1,25 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello from Spring!";
+    }
+
+/*    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }*/
+/*
+    @GetMapping("/increment/{number}")
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + "!";
+    }*/
+
+}
